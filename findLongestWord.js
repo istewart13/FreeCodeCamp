79fd6237e883ad2f9a99457daa471093eaa670d3
Iain <iain.stewart08@gmail.com>
2017-01-17 10:31:31 +0000

    Update findLongestWord.js

--- findLongestWord.js
+++ findLongestWord.js
@@ -4,10 +4,10 @@
 
 function findLongestWord(str) {
   var strArray = str.split(" ");
-  str.sort(function(a, b) {
+  strArray.sort(function(a, b) {
     return b.length - a.length;
   });
-  return str[0].length;
+  return strArray[0].length;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
